409de832a7ffcc15ca30466342594ab21798c8a4
melanie-09 <143124193+melanie-09@users.noreply.github.com>
2025-01-04 18:01:26 -0500

    beginning about page

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h1>About me</h1>
+            <p>Melanie Navaratna</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
